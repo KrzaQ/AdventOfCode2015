af6a7cb6a424bd8c70f7535286ab36037eb0faa2
krzaq <git@krzaq.cc>
2015-12-02 09:34:12 +0100

    typo fix + small adjustment

--- day02/main.rb
+++ day02/main.rb
@@ -1,6 +1,6 @@
 T = File.read('data.txt')
 
-R = T.lines.map{|l| /(\d+)x(\d+)x(\d+)/.match l}.map{|m| [m[1].to_i, m[2].to_i, m[3].to_i]}
+R = T.lines.map{|l| /(\d+)x(\d+)x(\d+)/.match l}.map{|m| [m[1].to_i, m[2].to_i, m[3].to_i].sort}
 
-puts "Task 1: %d" % R.map{|a| a.sort }.map{|a,b,c| 3*a*b + 2*a*c + 2*b*c }.inject(:+)
-puts "Task 1: %d" % R.map{|a| a.sort }.map{|a,b,c| 2*a+2*b + a*b*c}.inject(:+)
+puts "Task 1: %d" % R.map{|a,b,c| 3*a*b + 2*a*c + 2*b*c }.inject(:+)
+puts "Task 2: %d" % R.map{|a,b,c| 2*a+2*b + a*b*c}.inject(:+)
