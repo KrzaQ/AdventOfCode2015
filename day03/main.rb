b54a2a17ebf4616a991075b7d46122695dd25e01
krzaq <git@krzaq.cc>
2015-12-03 11:23:04 +0100

    Day 3 done

---
+++ day03/main.rb
@@ -0,0 +1,36 @@
+T = File.read("data.txt")
+
+def simPath(path)
+	path.each_char.inject({current: [0,0], total: { [0,0] => 1 }}){ |memo,c|
+		point = [{
+			'^' => [ 0,  1],
+			'>' => [ 1,  0],
+			'v' => [ 0, -1],
+			'<' => [-1,  0]
+		}[c], memo[:current]].transpose.map{ |n| n.inject(:+) }
+
+		memo[:total][point] = memo[:total].fetch(point, 0)+1
+		{
+			current: point,
+			total: memo[:total]
+		}
+	}
+end
+
+
+ThisYear = simPath(T)
+
+NextYearSantasPath, NextYearRoboSantasPath = T.each_char.each_with_index.map{ |c,i|
+	[c,i]
+}.group_by{ |c, i|
+	i % 2
+}.map{|unused, arr|
+	arr.map{ |c, i| c}.join
+}
+
+NextYearSanta = simPath(NextYearSantasPath)
+NextYearRoboSanta = simPath(NextYearRoboSantasPath)
+
+puts "Task 1: %s" % ThisYear[:total].size
+puts "Task 2: %s" % NextYearSanta[:total].merge(NextYearRoboSanta[:total]){ |k, v1, v2| v1 + v2 }.size
+
