0d4def921c7b77c0f556dc0d6731dc049019521e
krzaq <git@krzaq.cc>
2015-12-04 15:54:24 +0100

    Prettification as suggested by @hsz

--- day04/main.py
+++ day04/main.py
@@ -1,12 +1,9 @@
 import hashlib
 
-def Solve(n):
-	x = 0
-	while True:
-		hex = hashlib.md5(('yzbqklnj%d' % x).encode('utf-8')).hexdigest()
-		if hex[0:n] == '0' * n:
-			return x
-		x = x+1
+def Solve(n, x = 0):
+	while not hashlib.md5(('yzbqklnj%d' % x).encode('utf-8')).hexdigest().startswith('0' * n):
+		x += 1
+	return x
 
 print('Task 1: %d' % Solve(5))
 print('Task 2: %d' % Solve(6))
