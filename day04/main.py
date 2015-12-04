e8cbae3d1135bfb8c6814b43f59ebf2e31f1c208
krzaq <git@krzaq.cc>
2015-12-04 15:39:53 +0100

    Replaced ranged for with a while loop.

--- day04/main.py
+++ day04/main.py
@@ -1,10 +1,12 @@
 import hashlib
 
 def Solve(n):
-	for x in range(1, 999999999999):
+	x = 0
+	while True:
 		hex = hashlib.md5(('yzbqklnj%d' % x).encode('utf-8')).hexdigest()
 		if hex[0:n] == '0' * n:
 			return x
+		x = x+1
 
 print('Task 1: %d' % Solve(5))
 print('Task 2: %d' % Solve(6))
