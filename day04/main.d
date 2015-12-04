6d40cf781dbf25d34c987061050a6dcfa311277e
krzaq <git@krzaq.cc>
2015-12-04 13:27:46 +0100

    Prettification

--- day04/main.d
+++ day04/main.d
@@ -8,6 +8,9 @@ import std.string;
 
 void main()
 {
-	"Task 1: %s".format(iota(ulong.min, ulong.max).find!(n => "yzbqklnj%s".format(n).md5Of.toHexString.array.startsWith("00000")).front).writeln;
-	"Task 2: %s".format(iota(ulong.min, ulong.max).find!(n => "yzbqklnj%s".format(n).md5Of.toHexString.array.startsWith("000000")).front).writeln;
+	auto findSolution = function ulong(immutable string s){
+		return iota(ulong.min, ulong.max).find!(n => "yzbqklnj%s".format(n).md5Of.toHexString.array.startsWith(s)).front;
+	};
+	"Task 1: %s".format(findSolution("00000")).writeln;
+	"Task 2: %s".format(findSolution("000000")).writeln;
 }
