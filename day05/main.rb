1e97d8d08fcd812cd3920b04c97c11da5057ffa2
krzaq <git@krzaq.cc>
2015-12-05 17:22:19 +0100

    Regex optimization

--- day05/main.rb
+++ day05/main.rb
@@ -1,3 +1,3 @@
 T = File.read('data.txt').lines
-puts 'Task 1: %s' % T.select{|l| /[aeuio].*[aeuio].*[aeuio]/ =~ l}.select{|l| /(.)(\1)/ =~ l}.reject{|l| /(?:ab|cd|pq|xy)/ =~ l}.count
-puts 'Task 2: %s' % T.select{|l| /(..).*(\1)/ =~ l}.select{|l| /(.).(\1)/ =~ l}.count
+puts 'Task 1: %s' % T.select{|l| /(?:[aeuio].*){3}/ =~ l}.select{|l| /(.)\1/ =~ l}.reject{|l| /ab|cd|pq|xy/ =~ l}.count
+puts 'Task 2: %s' % T.select{|l| /(..).*\1/ =~ l}.select{|l| /(.).\1/ =~ l}.count
