89a29ecca0a7aea0b9ee12176460275542db5d6e
krzaq <git@krzaq.cc>
2015-12-05 10:53:55 +0100

    Day 05 Ruby solution added

---
+++ day05/main.rb
@@ -0,0 +1,3 @@
+T = File.read('data.txt').lines
+puts 'Task 1: %s' % T.select{|l| /[aeuio].*[aeuio].*[aeuio]/ =~ l}.select{|l| /(.)(\1)/ =~ l}.reject{|l| /(?:ab|cd|pq|xy)/ =~ l}.count
+puts 'Task 2: %s' % T.select{|l| /(..).*(\1)/ =~ l}.select{|l| /(.).(\1)/ =~ l}.count
