9d1fa85b1a2b70ba56bf6f3effd8492cbd5a3799
krzaq <git@krzaq.cc>
2015-12-08 21:03:02 +0100

    shortening

--- day02/main.rb
+++ day02/main.rb
@@ -1,6 +1,4 @@
-T = File.read('data.txt')
-
-R = T.lines.map{|l| /(\d+)x(\d+)x(\d+)/.match l}.map{|m| m[1..3].map{|n| n.to_i}.sort}
+R = File.read('data.txt').lines.map{|l| l.split('x').map{|n| n.to_i}.sort}
 
 puts "Task 1: %d" % R.map{|a,b,c| 3*a*b + 2*a*c + 2*b*c }.inject(:+)
 puts "Task 2: %d" % R.map{|a,b,c| 2*a+2*b + a*b*c}.inject(:+)
