6db02a1b44eb1a02995ec190659d426c48381173
krzaq <git@krzaq.cc>
2015-12-08 19:52:14 +0100

    Minifying the day 8 Ruby solution

--- day08/main.rb
+++ day08/main.rb
@@ -1,5 +1,3 @@
 D = File.read('data.txt').split("\n")
-T = { '"' => '\\"', '\\' => '\\\\'}
-
 puts 'Task 1: %s' % D.map{|l| l.size - eval(l).size}.inject(:+)
-puts 'Task 2: %s' % D.map{|l| l.gsub(Regexp.union(T.keys), T).size + 2 - l.size }.inject(:+)
+puts 'Task 2: %s' % D.map{|l| l.inspect.size - l.size }.inject(:+)
