659b32a165c0628490f994213ccf982e209403b0
krzaq <git@krzaq.cc>
2015-12-10 18:40:03 +0100

    removed superfluous code

--- day10/main.rb
+++ day10/main.rb
@@ -1,3 +1,3 @@
-Elvify = lambda{|d| (1..d).inject('3113322113'){ |t,d| t.to_s.each_char.chunk{|c| c}.map{|q, a| [a.size.to_s, a.first]}.flatten.join } }
+Elvify = lambda{|d| (1..d).inject('3113322113'){ |t| t.each_char.chunk{|c| c}.map{|q, a| [a.size.to_s, a.first]}.flatten.join } }
 puts 'Task 1: %s' % Elvify.call(40).size
 puts 'Task 2: %s' % Elvify.call(50).size
