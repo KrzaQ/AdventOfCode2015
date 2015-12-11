37ffecbe521b212f6f4067af3d6b756d45cd1b45
krzaq <git@krzaq.cc>
2015-12-11 12:09:30 +0100

    Shortening strongly inspired by @0x0dea 's solution: https://pay.reddit.com/r/adventofcode/comments/3wbzyv/day_11_solutions/cxv1vx5

--- day11/main.rb
+++ day11/main.rb
@@ -1,8 +1,5 @@
-def increment(n) (n.to_i(36)+1).to_s(36).gsub('0', 'a') end
-def passwords(pass) Enumerator.new do |y| loop do pass = increment(pass); y << pass; end end end
-def firstReq(n) n.each_char.each_cons(3).select{|a,b,c| a.ord == b.ord-1 && b.ord == c.ord-1 }.size > 0 end
-def secondReq(n) (n =~ /i|l|o/) == nil end
-def thirdReq(n) (n =~ /(\w)\1.*(\w)\2/) != nil end
-def reqs(n) [firstReq(n), secondReq(n), thirdReq(n)] end
+def passwords(pass) Enumerator.new do |y| loop do y << (pass = pass.succ); end end end
+def reqs(n) FirstReq =~ n && /i|l|o/ !~ n && n.scan(/(\w)\1/).size > 1 end
+FirstReq = Regexp.union (?a..?z).each_cons(3).map(&:join)
 
-puts "Task 1: %s\nTask 2: %s" % passwords('hxbxwxba').lazy.select{ |pass| reqs(pass).all? }.take(2).to_a
+puts "Task 1: %s\nTask 2: %s" % passwords('hxbxwxba').lazy.select{ |pass| reqs(pass) }.take(2).to_a
