cdcf0ea1ae5abf0d86e689398ea74e3d85010e65
krzaq <git@krzaq.cc>
2015-12-16 16:48:15 +0100

    typo fixes

--- day16/main.rb
+++ day16/main.rb
@@ -1,4 +1,4 @@
-Sue = { children: 3, cats: 7, samoyeds: 2, pomeranians: 3,  akitas: 0, vizslas: 0, goldfish: 5, trees: 3, cars: 2, perfumes: 1}
+Sue = { children: 3, cats: 7, samoyeds: 2, pomeranians: 3, akitas: 0, vizslas: 0, goldfish: 5, trees: 3, cars: 2, perfumes: 1}
 S = File.read('data.txt')
 .scan(/Sue (\d+): (\w+): (\d+), (\w+): (\d+), (\w+): (\d+)/)
 .map{|arr| arr[1..-1].each_slice(2).inject({sue: arr.first.to_i}){|t, a| t[a.first.to_sym] = a.last.to_i; t}}
@@ -18,5 +18,5 @@ def match2(sue, partial, k)
 	end
 end	
 
-puts 'Task 1: %s' %  S.select{|s| sueMatchesPartial(Sue, s, :match1 )}.first[:sue]
-puts 'Task 2: %s' %  S.select{|s| sueMatchesPartial(Sue, s, :match2 )}.first[:sue]
+puts 'Task 1: %s' %  S.select{|s| sueMatchesPartial(Sue, s, :match1)}.first[:sue]
+puts 'Task 2: %s' %  S.select{|s| sueMatchesPartial(Sue, s, :match2)}.first[:sue]
