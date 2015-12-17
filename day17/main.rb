50adc6c3a0787c8541248ecb8e18b639963dda24
krzaq <git@krzaq.cc>
2015-12-17 13:02:30 +0100

    speedups

--- day17/main.rb
+++ day17/main.rb
@@ -1,8 +1,7 @@
-D = File.read('data.txt').lines.map(&:to_i)
+D = File.read('data.txt').lines.map(&:to_i).sort.reverse
 
 def combinationsThatSumTo(max, arr)
-	arr.select{|n| n <= max}.sort.reverse
-	return [] if arr.size == 0
+	arr.select{|n| n <= max}
 	Enumerator.new do |y|
 		arr.each_with_index do |n, i|
 			combinationsThatSumTo(max - n, arr[(i+1)..-1]).each do |arr|
@@ -13,5 +12,7 @@ def combinationsThatSumTo(max, arr)
 	end
 end
 
-puts 'Task 1: %s' % combinationsThatSumTo(150, D).count
-puts 'Task 2: %s' % combinationsThatSumTo(150, D).group_by{|n| n.size}.to_a.min { |a,b| a.first <=> b.first }[1].size
+Results = combinationsThatSumTo(150, D).to_a
+
+puts 'Task 1: %s' % Results.size
+puts 'Task 2: %s' % Results.group_by{|n| n.size}.min{|a,b| a.first <=> b.first }[1].size
